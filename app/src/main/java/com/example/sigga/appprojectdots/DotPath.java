5598b6e2a6eb1dec9d873846949679f28207bc16
sixxa29 <siggahrafns@gmail.com>
2015-09-19 21:20:21 +0000

    !!!!crayForKris

--- app/src/main/java/com/example/sigga/appprojectdots/DotPath.java
+++ app/src/main/java/com/example/sigga/appprojectdots/DotPath.java
@@ -1,16 +1,11 @@
 package com.example.sigga.appprojectdots;
 
-import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
-import android.graphics.RectF;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.OvalShape;
 
+import java.security.PublicKey;
 import java.util.ArrayList;
-import java.util.Random;
 
 /**
  * Created by sigga on 15.9.2015.
@@ -18,83 +13,39 @@ import java.util.Random;
 public class DotPath {
 
     private ArrayList<Point> m_path = new ArrayList<Point>();
-    private Path drawPath = new Path();
-    private Paint paintPath  = new Paint();
-    private int color;
-
 
     public void append(Point point) {
         int idx = m_path.indexOf(point);
-        if ( idx >= 0 ) {
-            for ( int i=m_path.size()-1; i > idx; --i ) {
+        if (idx >= 0) {
+            for (int i = m_path.size() - 1; i > idx; --i) {
                 m_path.remove(i);
             }
-        }
-        else {
+        } else {
             m_path.add(point);
         }
+
     }
 
-    public ArrayList<Point> getCoordinates() {
-        return m_path;
-    }
-
-    public void movePath(int x, int y) {
-
-        drawPath.moveTo(x,y);
-    }
-
-    public void linePath(int x, int y){
-
-        drawPath.lineTo(x, y);
-    }
-
-    public Path getPath(){
-        return drawPath;
-    }
-
-    public Point getLast(){
+    public Point getLast() {
         return m_path.get(m_path.size() - 1);
     }
 
-    public void setPaintPath(int color){
-        // Set Color
-        this.color = color;
-        paintPath.setColor(Color.CYAN);
-
-        // Cell path paint settings
-        paintPath.setStyle(Paint.Style.STROKE);
-        paintPath.setStrokeWidth(32);
-
-        paintPath.setStrokeCap(Paint.Cap.ROUND);
-        paintPath.setStrokeJoin(Paint.Join.ROUND);
-        paintPath.setAntiAlias(true);
-
+    public Integer lastInt(){
+        return m_path.size() - 1;
     }
 
-  /*  public void drawPaths(Canvas canvas, int x, int y){
-        //ArrayList<Point> point = m_path.getCoordinates();
-        Point index = m_path.get(0);
-
-        if(m_path.isEmpty()){ return; }
-
-        drawPath.moveTo(x, y);
-        for(int i = 1; i < m_path.size(); ++i) {
-
-            index = m_path.get(i);
-            x = colToX(index.x) + m_cellWidth / 2;
-            y = rowToY(index.y) + m_cellHeight / 2;
-
-            drawPath.lineTo(x, y);
-        }
-    }*/
-
-    public int getPaintPath(){
-        return paintPath.getColor();
+    public Point get(int i){
+        return m_path.get(i);
     }
+
+    public Integer getSize() { return m_path.size(); }
 
     public void reset() {
         m_path.clear();
+    }
+
+    public boolean contains(Point point) {
+        return m_path.contains(point);
     }
 
     public boolean isEmpty() {
